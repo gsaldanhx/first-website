ddfa36058f6ba814f6014af4555fb803a3d2399e
guilherme <guigsaldanha@gmail.com>
2022-09-19 00:48:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+
+const relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+
+    if (hr < 10) hr = '0' + hr;
+    if (min < 10) min = '0' + min;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+
+})
+
